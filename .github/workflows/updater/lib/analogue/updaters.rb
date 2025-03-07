993b30195fafe2b0586e829b0616e94a659f3645
Joshua Campbell <joshcampbell191@gmail.com>
2025-03-07 08:11:22 -0500

    Added missing updaters section

--- .github/workflows/updater/lib/analogue/updaters.rb
+++ .github/workflows/updater/lib/analogue/updaters.rb
@@ -3,18 +3,32 @@
 module Analogue
   # Describes updaters configuration.
   class Updaters
-    attr_reader :license
+    attr_reader :license, :previous
 
     def initialize(updaters)
       @license = License.new(updaters.license)
+      @previous = (updaters.previous || []).map do |previous|
+        Previous.new(previous)
+      end
     end
 
-    # Describes a single license.
+    # Describes a license.
     class License
       attr_reader :filename
 
       def initialize(license)
         @filename = license.filename
+      end
+    end
+
+    # Describes a previous core.
+    class Previous
+      attr_reader :shortname, :author, :platform_id
+
+      def initialize(previous)
+        @shortname = previous.shortname
+        @author = previous.author
+        @platform_id = previous.platform_id
       end
     end
   end
